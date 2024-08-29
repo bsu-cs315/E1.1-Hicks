39d168e7a1ab385b28e9d58687ec840701d1f9af
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2024-08-29 15:04:59 -0400

    Users can modify power

--- ball.gd
+++ ball.gd
@@ -1,6 +1,18 @@
 extends RigidBody2D
+@export var power_change_rate=100
+@export var power=200
+
 
 func _process(_delta: float) -> void:
+	
+	if Input.is_action_pressed("increase_power"):
+		power+=10* _delta
+	elif Input.is_action_pressed("decrease_power"):
+		power -= 10* _delta
+	
+	print(power)
+	
+	
 	if Input.is_action_just_pressed("launch"):
-		var impulse=Vector2(1,-1)*200
-		apply_impulse(impulse,Vector2.ZERO)
+		var impulse=Vector2(1,0)*power
+		apply_impulse(impulse.rotated(-TAU/8))
