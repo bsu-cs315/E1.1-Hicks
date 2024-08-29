5c2569a087f9ff4ec831d847191bb044fb27626f
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2024-08-29 15:17:18 -0400

    Mapping Issue was fixed

--- ball.gd
+++ ball.gd
@@ -1,16 +1,13 @@
 extends RigidBody2D
-@export var power_change_rate=100
 @export var power=200
+@export var power_change_rate=100
 
 
-func _process(_delta: float) -> void:
-	
-	if Input.is_action_pressed("increase_power"):
-		power+=10* _delta
-	elif Input.is_action_pressed("decrease_power"):
-		power -= 10* _delta
-	
-	print(power)
+
+func _process(delta: float) -> void:
+	var power_axis=Input.get_axis("decrease_power", "increase_power")
+	power+=power_axis * power_change_rate * delta	
+	power=clampf(power,0,1000)
 	
 	
 	if Input.is_action_just_pressed("launch"):
