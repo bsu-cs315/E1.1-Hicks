4720a10eb559f12cd8bd394802d7baf8a009a96b
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2024-09-02 19:56:12 -0400

    Direction Vector has been fixed. The movement is corresponding to button pushed.

--- ball.gd
+++ ball.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	
 	
 	if Input.is_action_just_pressed("launch"):
-		var x_cor=Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-		var y_cor=Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
+		var x_cor=Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		var y_cor=Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 		var impulse=Vector2(x_cor,y_cor)*power
 		apply_impulse(impulse.rotated(-TAU/8))
